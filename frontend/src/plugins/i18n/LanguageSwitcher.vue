c9fbb619f4add38fd96084ad322ae0abf3a4a3b8
Antonio Oliva <antonio.oliva@europassistance.it>
2024-03-17 19:35:03 +0100

    upper case language

--- frontend/src/plugins/i18n/LanguageSwitcher.vue
+++ frontend/src/plugins/i18n/LanguageSwitcher.vue
@@ -1,7 +1,7 @@
 <template>
     <li class="nav-item">
         <select v-model="$i18n.locale" class="form-select">
-            <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">{{ locale }}</option>
+            <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">{{ locale.toUpperCase() }}</option>
         </select>             
     </li> 
 </template>
